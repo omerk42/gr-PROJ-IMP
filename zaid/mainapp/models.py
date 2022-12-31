4c957e32289123aac2847ab23fec5d14a4b5e0ad
stren-12 <smagsf@gmail.com>
2022-12-31 18:59:12 +0300

    Add some value

--- zaid/mainapp/models.py
+++ zaid/mainapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Status(models.Model):
+    class Meta:
+        # https://english.stackexchange.com/questions/877/what-is-the-plural-form-of-status
+        db_table = "statuses"
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255,null=False,unique=True,default="default")
+class Auction(models.Model):
+    class Meta:
+        db_table = "auctions"
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255,null=False)
+    description = models.TextField(max_length=2000,null=False)
+    status = models.ForeignKey("Status",on_delete=models.CASCADE)
+    main_image = models.FileField(upload_to='uploads/')
+    accepted_bid = models.ForeignKey("Bid",on_delete=models.CASCADE,null=True)
+    created_by  = models.ForeignKey("users.user",on_delete=models.CASCADE)
+    created_at  = models.DateTimeField(auto_now_add=True)
+
+
+class Bid(models.Model):
+    class Meta:
+        db_table = "bids"
+    id = models.AutoField(primary_key=True)
+    price = models.DecimalField(max_digits=14,decimal_places=2)
+    created_by  = models.ForeignKey("users.user",on_delete=models.CASCADE)
+    created_at  = models.DateTimeField(auto_now_add=True)
